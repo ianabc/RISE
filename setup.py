a89051065e792d2ff32d23482bf3b02c8009cb4a
Ian Allison <iana@pims.math.ca>
2015-11-21 00:09:09 +0000

    Added system installation option.
    This is just a band-aid, the RISE project have promised pip and conda packages
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from notebook.services.config import ConfigManager
 
 livereveal_dir = os.path.join(os.path.dirname(__file__), 'livereveal')
 
-def install(use_symlink=False, enable=True):
+def install(use_symlink=False, enable=True, user=True):
     # Install the livereveal code.
     install_nbextension(livereveal_dir, symlink=use_symlink,
-                        overwrite=use_symlink, user=True)
+                        overwrite=use_symlink, user=user)
 
     if enable:
         cm = ConfigManager()
@@ -27,11 +27,14 @@ def main():
                                 help="Install livereveal  as a symlink to the source.")
     install_parser.add_argument('--no-enable', action='store_true',
                                 help="Install but don't enable the extension.")
+    install_parser.add_argument('--system', action='store_true',
+				help="Install livereveal as a system wide resource.")
 
     args = parser.parse_args(sys.argv[1:])
 
     install(use_symlink=args.develop,
-            enable=(not args.no_enable))
+            enable=(not args.no_enable),
+            user=(not args.system))
 
 
 if __name__ == '__main__':
